59fed138ad9c6062121697926ec3aa00154f8d6a
Mohitz07 <mohitdhenge.1@gmail.com>
2025-08-02 09:42:02 +0530

    Pushing to github

---
+++ src/main/java/com/project/KisanSaathi/KisanSaathiApplication.java
@@ -0,0 +1,13 @@
+package com.project.KisanSaathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisanSaathiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KisanSaathiApplication.class, args);
+	}
+
+}
